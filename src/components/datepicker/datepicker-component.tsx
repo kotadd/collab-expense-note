ba19c64261574133df1cd8cfb88aa6d5a83f76a8
Kota Nishinaka <private2030kota@gmail.com>
2020-03-28 21:04:57 +0900

    Fix Datapicker validation

--- src/components/datepicker/datepicker-component.tsx
+++ src/components/datepicker/datepicker-component.tsx
@@ -5,22 +5,22 @@ type DatePickerType = {
   value: Date
   onChange: () => void
 }
-class DatePicker extends React.Component {
-  render() {
-    const { value, onChange } = this.props as DatePickerType
-    return (
-      <>
-        <DateTimePicker
-          value={value}
-          locale="ja-JA"
-          mode="date"
-          is24Hour={true}
-          display="default"
-          onChange={onChange}
-        />
-      </>
-    )
-  }
+const DatePicker: React.FC<DatePickerType> = ({
+  value,
+  onChange
+}: DatePickerType) => {
+  return (
+    <>
+      <DateTimePicker
+        value={value}
+        locale="ja-JA"
+        mode="date"
+        is24Hour={true}
+        display="default"
+        onChange={onChange}
+      />
+    </>
+  )
 }
 
 export default DatePicker
