204053f65ec39858fba14f13876460fda97cf6a6
Kota Nishinaka <knishina@yahoo-corp.jp>
2020-05-23 16:39:16 +0900

    Add prod config

--- repository/firebase/firebase.config.ts
+++ repository/firebase/firebase.config.ts
@@ -8,3 +8,14 @@ export const config = {
   appId: '1:661228793540:web:40a094d31c50d5d961a391',
   measurementId: 'G-1SQD65LYH7',
 }
+
+export const prodConfig = {
+  apiKey: 'AIzaSyCE4WP3AFwxFY0kbpZttEu54el8WfRW4yk',
+  authDomain: 'cen-db.firebaseapp.com',
+  databaseURL: 'https://cen-db.firebaseio.com',
+  projectId: 'cen-db',
+  storageBucket: 'cen-db.appspot.com',
+  messagingSenderId: '360148663219',
+  appId: '1:360148663219:web:04876693eded409b8e29c6',
+  measurementId: 'G-TP2SDSHV3N',
+}
