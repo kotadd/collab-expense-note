f6c3bcfc688bda6e331686c2b0f3622379de065c
Kota Nishinaka <knishina@yahoo-corp.jp>
2020-05-27 09:22:19 +0900

    Remove payment redux

--- src/redux/payment/payment.selector.ts
+++
@@ -1,5 +0,0 @@
-import { PaymentsReduxProps } from './payment.reducer'
-
-export const unsubscribedPaymentsSelector: (
-  state: PaymentsReduxProps
-) => () => void = (state) => state.payment.unsubscribedPayments
